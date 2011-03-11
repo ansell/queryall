602804c55d2da01b005a943ec1263fa9ca252a11
Peter Ansell <p_ansell@users.sourceforge.net>
2011-03-11 18:49:24 +1000

    add add* methods to Profile

--- queryall-api/src/main/java/org/queryall/Profile.java
+++ queryall-api/src/main/java/org/queryall/Profile.java
@@ -35,26 +35,44 @@ public abstract class Profile implements BaseQueryAllInterface, Comparable<Profi
     
     public abstract Collection<org.openrdf.model.URI> getProfileAdministrators();
     
+    
+    public abstract void addIncludeProvider(org.openrdf.model.URI includeProvider);
+
     public abstract void setIncludeProviders(Collection<org.openrdf.model.URI> includeProviders);
     
     public abstract Collection<org.openrdf.model.URI> getIncludeProviders();
     
+
+    public abstract void addExcludeProvider(org.openrdf.model.URI excludeProvider);
+
     public abstract void setExcludeProviders(Collection<org.openrdf.model.URI> excludeProviders);
     
     public abstract Collection<org.openrdf.model.URI> getExcludeProviders();
     
+
+    public abstract void addIncludeQuery(org.openrdf.model.URI includeQuery);
+
     public abstract void setIncludeQueries(Collection<org.openrdf.model.URI> includeQueries);
     
     public abstract Collection<org.openrdf.model.URI> getIncludeQueries();
     
+
+    public abstract void addExcludeQuery(org.openrdf.model.URI excludeQuery);
+
     public abstract void setExcludeQueries(Collection<org.openrdf.model.URI> excludeQueries);
     
     public abstract Collection<org.openrdf.model.URI> getExcludeQueries();
+
+
+    public abstract void addIncludeRdfRule(org.openrdf.model.URI includeRdfRule);
 
     public abstract void setIncludeRdfRules(Collection<org.openrdf.model.URI> includeRdfRules);
     
     public abstract Collection<org.openrdf.model.URI> getIncludeRdfRules();
     
+
+    public abstract void addExcludeRdfRule(org.openrdf.model.URI excludeRdfRule);
+
     public abstract void setExcludeRdfRules(Collection<org.openrdf.model.URI> excludeRdfRules);
     
     public abstract Collection<org.openrdf.model.URI> getExcludeRdfRules();
