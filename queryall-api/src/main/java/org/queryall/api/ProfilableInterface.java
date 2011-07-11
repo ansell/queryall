c02a8e79e335505687402ab2406af6fdc0eb8f57
Peter Ansell <p_ansell@users.sourceforge.net>
2011-07-11 14:57:07 +1000

    add some documentation for ProfilableInterface

--- queryall-api/src/main/java/org/queryall/api/ProfilableInterface.java
+++ queryall-api/src/main/java/org/queryall/api/ProfilableInterface.java
@@ -4,14 +4,33 @@ import java.util.List;
 
 import org.openrdf.model.URI;
 
+/**
+ * This interface provides the basis for profiles to be used to include or exclude objects
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
 public interface ProfilableInterface extends BaseQueryAllInterface
 {
-
+	/**
+	 * @return A URI indicating whether this object should have include or exclude instructions processed first
+	 */
     public abstract URI getProfileIncludeExcludeOrder();
 
+    /**
+     * @param profileIncludeExcludeOrder A URI indicating whether this object should have include or exclude instructions processed first
+     */
     public abstract void setProfileIncludeExcludeOrder(
             URI profileIncludeExcludeOrder);
     
+    /**
+     * This method determines whether this class should be included or excluded, 
+     * based on the pre-ordered profile list and the parameters which indicate 
+     * whether implicit inclusions are allowed and whether this method will 
+     * match if no profiles match at all
+     * @param orderedProfileList a pre sorted list of profiles that will be used in sequence to determine whether this object will be used
+     * @param allowImplicitInclusions true if implicit inclusions are acceptable, and false if implicit inclusions should be ignored and the profile list should be processed until and explicit match is found
+     * @param includeNonProfileMatched true if this object should be included even if no profiles matched, and false if there were no matches, including implicit inclusions if they are acceptable
+     * @return true if this object should be used and false if it should not
+     */
     public abstract boolean isUsedWithProfileList(List<Profile> orderedProfileList, boolean allowImplicitInclusions, boolean includeNonProfileMatched);
 
 }
