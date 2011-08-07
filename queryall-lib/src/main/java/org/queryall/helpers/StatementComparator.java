d0255cf8309a7d78e4e18315b1eeee6db5a3416b
Peter Ansell <p_ansell@yahoo.com>
2011-08-07 10:01:47 +1000

    update StatementComparator and add ValueComparator, both of which are designed to handle null contexts in a uniform manner

--- queryall-lib/src/main/java/org/queryall/helpers/StatementComparator.java
+++ queryall-lib/src/main/java/org/queryall/helpers/StatementComparator.java
@@ -5,20 +5,25 @@ import java.util.Comparator;
 import org.openrdf.model.Statement;
 
 /**
- * Implements a Comparator for OpenRDF Statements using the order Subject->Predicate->Object->Context
+ * Implements a Comparator for OpenRDF Statements 
+ * using the order Subject->Predicate->Object->Context
  * 
  * @author Peter Ansell p_ansell@yahoo.com
  */
 public class StatementComparator implements Comparator<Statement>
 {
+	public final static int BEFORE = -1;
 	public final static int EQUALS = 0;
+	public final static int AFTER = 1;
 
 	@Override
 	public int compare(Statement first, Statement second)
 	{
-		if(first.equals(second))
+		// Cannot use Statement.equals as it does not take Context into account, 
+		// but can check for reference equality (==)
+		if(first == second)
 		{
-			return 0;
+			return EQUALS;
 		}
 		
 		if(first.getSubject().equals(second.getSubject()))
@@ -27,21 +32,40 @@ public class StatementComparator implements Comparator<Statement>
 			{
 				if(first.getObject().equals(second.getObject()))
 				{
-					return first.getContext().stringValue().compareTo(second.getContext().stringValue());
+					// Context is the only part of a statement that should legitimately be null
+					if(first.getContext() == null)
+					{
+						if(second.getContext() == null)
+						{
+							return EQUALS;
+						}
+						else
+						{
+							return BEFORE;
+						}
+					}
+					else if(second.getContext() == null)
+					{
+						return AFTER;
+					}
+					else
+					{
+						return new ValueComparator().compare(first.getContext(), second.getContext());
+					}
 				}
 				else
 				{
-					return first.getObject().stringValue().compareTo(second.getObject().stringValue());
+					return new ValueComparator().compare(first.getObject(), second.getObject());
 				}
 			}
 			else
 			{
-				return first.getPredicate().stringValue().compareTo(second.getPredicate().stringValue());
+				return new ValueComparator().compare(first.getPredicate(), second.getPredicate());
 			}
 		}
 		else
 		{
-			return first.getSubject().stringValue().compareTo(second.getSubject().stringValue());
+			return new ValueComparator().compare(first.getSubject(), second.getSubject());
 		}
 	}
 
