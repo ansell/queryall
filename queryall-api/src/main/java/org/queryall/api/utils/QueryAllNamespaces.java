f6efc915946fa48e0d152dbaa7008ec44125a226
Peter Ansell <p_ansell@yahoo.com>
2011-08-08 12:58:42 +1000

    manually change QueryAllNamespaces so it looks good after automated cleanup

--- queryall-api/src/main/java/org/queryall/api/utils/QueryAllNamespaces.java
+++ queryall-api/src/main/java/org/queryall/api/utils/QueryAllNamespaces.java
@@ -11,18 +11,29 @@ import org.slf4j.LoggerFactory;
  */
 public enum QueryAllNamespaces
 {
-    NAMESPACEENTRY("Settings.DEFAULT_RDF_NAMESPACEENTRY_NAMESPACE", "ns", "Namespace Entries"), PROFILE(
-            "Settings.DEFAULT_RDF_PROFILE_NAMESPACE", "profile", "Profiles"), PROJECT(
-            "Settings.DEFAULT_RDF_PROJECT_NAMESPACE", "project", "Projects"), PROVENANCE(
-            "Settings.DEFAULT_RDF_PROVENANCE_NAMESPACE", "provenance", "Provenance Records"), PROVIDER(
-            "Settings.DEFAULT_RDF_PROVIDER_NAMESPACE", "provider", "Providers"), QUERY(
-            "Settings.DEFAULT_RDF_QUERY_NAMESPACE", "query", "Query Types"), QUERYBUNDLE(
-            "Settings.DEFAULT_RDF_QUERYBUNDLE_NAMESPACE", "querybundle", "Query Bundles"), RDFRULE(
-            "Settings.DEFAULT_RDF_RDFRULE_NAMESPACE", "rdfrule", "RDF Normalisation Rules"), RULETEST(
-            "Settings.DEFAULT_RDF_RULETEST_NAMESPACE", "ruletest", "Rule Tests"), STATISTICS(
-            "Settings.DEFAULT_RDF_STATISTICS_NAMESPACE", "statistics", "Statistics"), TEMPLATES(
-            "Settings.DEFAULT_RDF_TEMPLATE_NAMESPACE", "template", "Templates"), WEBAPPCONFIG(
-            "Settings.DEFAULT_RDF_WEBAPP_CONFIGURATION_NAMESPACE", "webapp_configuration",
+    NAMESPACEENTRY("Settings.DEFAULT_RDF_NAMESPACEENTRY_NAMESPACE", "ns", "Namespace Entries"),
+    
+    PROFILE("Settings.DEFAULT_RDF_PROFILE_NAMESPACE", "profile", "Profiles"),
+    
+    PROJECT("Settings.DEFAULT_RDF_PROJECT_NAMESPACE", "project", "Projects"),
+    
+    PROVENANCE("Settings.DEFAULT_RDF_PROVENANCE_NAMESPACE", "provenance", "Provenance Records"),
+    
+    PROVIDER("Settings.DEFAULT_RDF_PROVIDER_NAMESPACE", "provider", "Providers"),
+    
+    QUERY("Settings.DEFAULT_RDF_QUERY_NAMESPACE", "query", "Query Types"),
+    
+    QUERYBUNDLE("Settings.DEFAULT_RDF_QUERYBUNDLE_NAMESPACE", "querybundle", "Query Bundles"),
+    
+    RDFRULE("Settings.DEFAULT_RDF_RDFRULE_NAMESPACE", "rdfrule", "RDF Normalisation Rules"),
+    
+    RULETEST("Settings.DEFAULT_RDF_RULETEST_NAMESPACE", "ruletest", "Rule Tests"),
+    
+    STATISTICS("Settings.DEFAULT_RDF_STATISTICS_NAMESPACE", "statistics", "Statistics"),
+    
+    TEMPLATES("Settings.DEFAULT_RDF_TEMPLATE_NAMESPACE", "template", "Templates"),
+    
+    WEBAPPCONFIG("Settings.DEFAULT_RDF_WEBAPP_CONFIGURATION_NAMESPACE", "webapp_configuration",
             "Web Application Configurations");
     
     private static final Logger log = LoggerFactory.getLogger(QueryAllNamespaces.class);
