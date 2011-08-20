9d71f29986f15b9ee3da4ee8dd92e9cf351806c9
Peter Ansell <p_ansell@yahoo.com>
2011-08-20 21:55:46 +1000

    add more ProviderUtils tests

--- queryall-lib/src/test/java/org/queryall/utils/test/ProviderUtilsTest.java
+++ queryall-lib/src/test/java/org/queryall/utils/test/ProviderUtilsTest.java
@@ -35,6 +35,7 @@ import org.queryall.api.Provider;
 import org.queryall.api.QueryType;
 import org.queryall.api.utils.QueryAllNamespaces;
 import org.queryall.impl.ProfileImpl;
+import org.queryall.impl.ProviderImpl;
 import org.queryall.impl.QueryTypeImpl;
 import org.queryall.utils.ProviderUtils;
 import org.queryall.utils.RdfUtils;
@@ -54,6 +55,15 @@ public class ProviderUtilsTest
     private QueryType testDefaultProvidersQueryTypeFalse1;
     private List<Profile> emptyProfileList;
     private List<Profile> singleImplicitAllowAllProfileList;
+    private Collection<Collection<URI>> testNamespaces1;
+    private Collection<Collection<URI>> testNamespaces12;
+    private Collection<Collection<URI>> testNamespaces123;
+    private Collection<Collection<URI>> testNamespaces23;
+    private URI testQueryUri1;
+    private URI testNamespaceUri1;
+    private URI testNamespaceUri2;
+    private URI testNamespaceUri3;
+    private URI testNamespaceUri4;
 
     /**
      * @throws java.lang.Exception
@@ -86,6 +96,41 @@ public class ProviderUtilsTest
         
         singleImplicitAllowAllProfileList.add(testImplicitAllowAllProfile);
         
+        testQueryUri1 = this.testValueFactory.createURI("http://example.org/query:test-1");
+        
+        testNamespaceUri1 = this.testValueFactory.createURI("http://example.org/ns:test-1");
+        testNamespaceUri2 = this.testValueFactory.createURI("http://example.org/ns:test-2");
+        testNamespaceUri3 = this.testValueFactory.createURI("http://example.org/ns:test-3");
+        testNamespaceUri4 = this.testValueFactory.createURI("http://example.org/ns:test-4");
+        
+        Collection<URI> tempTestNamespace1 = new LinkedList<URI>();
+        tempTestNamespace1.add(testNamespaceUri1);
+        
+        Collection<URI> tempTestNamespace2 = new LinkedList<URI>();
+        tempTestNamespace2.add(testNamespaceUri2);
+        
+        Collection<URI> tempTestNamespace3 = new LinkedList<URI>();
+        tempTestNamespace3.add(testNamespaceUri3);
+        
+        Collection<URI> tempTestNamespace4 = new LinkedList<URI>();
+        tempTestNamespace4.add(testNamespaceUri4);
+        
+        this.testNamespaces1 = new LinkedList<Collection<URI>>();
+        this.testNamespaces12 = new LinkedList<Collection<URI>>();
+        this.testNamespaces123 = new LinkedList<Collection<URI>>();
+        this.testNamespaces23 = new LinkedList<Collection<URI>>();
+
+        this.testNamespaces1.add(tempTestNamespace1);
+        
+        this.testNamespaces12.add(tempTestNamespace1);
+        this.testNamespaces12.add(tempTestNamespace2);
+
+        this.testNamespaces123.add(tempTestNamespace1);
+        this.testNamespaces123.add(tempTestNamespace2);
+        this.testNamespaces123.add(tempTestNamespace3);
+
+        this.testNamespaces23.add(tempTestNamespace2);
+        this.testNamespaces23.add(tempTestNamespace3);
     }
     
     /**
@@ -118,6 +163,18 @@ public class ProviderUtilsTest
         
         this.emptyProfileList = null;
         this.singleImplicitAllowAllProfileList = null;
+        
+        this.testNamespaces1 = null;
+        this.testNamespaces12 = null;
+        this.testNamespaces123 = null;
+        this.testNamespaces23 = null;
+        
+        this.testQueryUri1 = null;
+        
+        this.testNamespaceUri1 = null;
+        this.testNamespaceUri2 = null;
+        this.testNamespaceUri3 = null;
+        this.testNamespaceUri4 = null;
     }
     
     /**
@@ -274,10 +331,68 @@ public class ProviderUtilsTest
      * Test method for {@link org.queryall.utils.ProviderUtils#getProvidersForNamespaceUris(java.util.Map, java.util.Collection, org.openrdf.model.URI)}.
      */
     @Test
-    @Ignore
     public void testGetProvidersForNamespaceUris()
     {
-        fail("Not yet implemented");
+        final InputStream nextInputStream = this.getClass().getResourceAsStream("/testconfigs/multipleNamespaceProviderTest-1.n3");
+        
+        try
+        {
+            Assert.assertNotNull("Could not find test file", nextInputStream);
+            
+            this.testRepositoryConnection.add(nextInputStream, "", RDFFormat.N3);
+            this.testRepositoryConnection.commit();
+            
+            
+            Map<URI, Provider> testProviders = RdfUtils.getProviders(testRepository);
+            
+            Assert.assertEquals(4, testProviders.size());
+            
+            Map<URI, Provider> allNamespaceMatchProviders = ProviderUtils.getProvidersForNamespaceUris(testProviders, testNamespaces1, QueryTypeImpl.getNamespaceMatchAllUri());
+            
+            Assert.assertEquals(3, allNamespaceMatchProviders.size());
+
+            allNamespaceMatchProviders = ProviderUtils.getProvidersForNamespaceUris(testProviders, testNamespaces12, QueryTypeImpl.getNamespaceMatchAllUri());
+            
+            Assert.assertEquals(2, allNamespaceMatchProviders.size());
+        
+            allNamespaceMatchProviders = ProviderUtils.getProvidersForNamespaceUris(testProviders, testNamespaces123, QueryTypeImpl.getNamespaceMatchAllUri());
+            
+            Assert.assertEquals(1, allNamespaceMatchProviders.size());
+        
+            allNamespaceMatchProviders = ProviderUtils.getProvidersForNamespaceUris(testProviders, testNamespaces23, QueryTypeImpl.getNamespaceMatchAllUri());
+            
+            Assert.assertEquals(2, allNamespaceMatchProviders.size());
+        
+
+            Map<URI, Provider> anyNamespaceMatchProviders = ProviderUtils.getProvidersForNamespaceUris(testProviders, testNamespaces1, QueryTypeImpl.getNamespaceMatchAnyUri());
+            
+            Assert.assertEquals(3, anyNamespaceMatchProviders.size());
+
+            anyNamespaceMatchProviders = ProviderUtils.getProvidersForNamespaceUris(testProviders, testNamespaces12, QueryTypeImpl.getNamespaceMatchAnyUri());
+            
+            Assert.assertEquals(4, anyNamespaceMatchProviders.size());
+        
+            anyNamespaceMatchProviders = ProviderUtils.getProvidersForNamespaceUris(testProviders, testNamespaces123, QueryTypeImpl.getNamespaceMatchAnyUri());
+            
+            Assert.assertEquals(4, anyNamespaceMatchProviders.size());
+        
+            anyNamespaceMatchProviders = ProviderUtils.getProvidersForNamespaceUris(testProviders, testNamespaces23, QueryTypeImpl.getNamespaceMatchAnyUri());
+            
+            Assert.assertEquals(3, anyNamespaceMatchProviders.size());
+        
+        }
+        catch(final RDFParseException ex)
+        {
+            Assert.fail("Found unexpected RDFParseException : " + ex.getMessage());
+        }
+        catch(final RepositoryException ex)
+        {
+            Assert.fail("Found unexpected RepositoryException : " + ex.getMessage());
+        }
+        catch(final IOException ex)
+        {
+            Assert.fail("Found unexpected IOException : " + ex.getMessage());
+        }
     }
     
     /**
@@ -304,10 +419,68 @@ public class ProviderUtilsTest
      * Test method for {@link org.queryall.utils.ProviderUtils#getProvidersForQueryTypeForNamespaceUris(java.util.Map, org.openrdf.model.URI, java.util.Collection, org.openrdf.model.URI)}.
      */
     @Test
-    @Ignore
     public void testGetProvidersForQueryTypeForNamespaceUris()
     {
-        fail("Not yet implemented");
+        final InputStream nextInputStream = this.getClass().getResourceAsStream("/testconfigs/multipleNamespaceProviderTest-1.n3");
+        
+        try
+        {
+            Assert.assertNotNull("Could not find test file", nextInputStream);
+            
+            this.testRepositoryConnection.add(nextInputStream, "", RDFFormat.N3);
+            this.testRepositoryConnection.commit();
+            
+            
+            Map<URI, Provider> testProviders = RdfUtils.getProviders(testRepository);
+            
+            Assert.assertEquals(4, testProviders.size());
+            
+            Map<URI, Provider> allNamespaceMatchProviders = ProviderUtils.getProvidersForQueryTypeForNamespaceUris(testProviders, testQueryUri1, testNamespaces1, QueryTypeImpl.getNamespaceMatchAllUri());
+            
+            Assert.assertEquals(3, allNamespaceMatchProviders.size());
+
+            allNamespaceMatchProviders = ProviderUtils.getProvidersForQueryTypeForNamespaceUris(testProviders, testQueryUri1, testNamespaces12, QueryTypeImpl.getNamespaceMatchAllUri());
+            
+            Assert.assertEquals(2, allNamespaceMatchProviders.size());
+        
+            allNamespaceMatchProviders = ProviderUtils.getProvidersForQueryTypeForNamespaceUris(testProviders, testQueryUri1, testNamespaces123, QueryTypeImpl.getNamespaceMatchAllUri());
+            
+            Assert.assertEquals(1, allNamespaceMatchProviders.size());
+        
+            allNamespaceMatchProviders = ProviderUtils.getProvidersForQueryTypeForNamespaceUris(testProviders, testQueryUri1, testNamespaces23, QueryTypeImpl.getNamespaceMatchAllUri());
+            
+            Assert.assertEquals(2, allNamespaceMatchProviders.size());
+        
+
+            Map<URI, Provider> anyNamespaceMatchProviders = ProviderUtils.getProvidersForQueryTypeForNamespaceUris(testProviders, testQueryUri1, testNamespaces1, QueryTypeImpl.getNamespaceMatchAnyUri());
+            
+            Assert.assertEquals(3, anyNamespaceMatchProviders.size());
+
+            anyNamespaceMatchProviders = ProviderUtils.getProvidersForQueryTypeForNamespaceUris(testProviders, testQueryUri1, testNamespaces12, QueryTypeImpl.getNamespaceMatchAnyUri());
+            
+            Assert.assertEquals(4, anyNamespaceMatchProviders.size());
+        
+            anyNamespaceMatchProviders = ProviderUtils.getProvidersForQueryTypeForNamespaceUris(testProviders, testQueryUri1, testNamespaces123, QueryTypeImpl.getNamespaceMatchAnyUri());
+            
+            Assert.assertEquals(4, anyNamespaceMatchProviders.size());
+        
+            anyNamespaceMatchProviders = ProviderUtils.getProvidersForQueryTypeForNamespaceUris(testProviders, testQueryUri1, testNamespaces23, QueryTypeImpl.getNamespaceMatchAnyUri());
+            
+            Assert.assertEquals(3, anyNamespaceMatchProviders.size());
+        
+        }
+        catch(final RDFParseException ex)
+        {
+            Assert.fail("Found unexpected RDFParseException : " + ex.getMessage());
+        }
+        catch(final RepositoryException ex)
+        {
+            Assert.fail("Found unexpected RepositoryException : " + ex.getMessage());
+        }
+        catch(final IOException ex)
+        {
+            Assert.fail("Found unexpected IOException : " + ex.getMessage());
+        }
     }
     
 }
