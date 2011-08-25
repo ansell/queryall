f68e13a5bd0ec217ec332f23ec13204b33498842
Peter Ansell <p_ansell@yahoo.com>
2011-08-25 10:19:27 +1000

    add sparql and xslt factory and parser's
    the current bug is that the *NormalisationRuleImpl classes are not being initialised, so the static code to create the NormalisatioinRuleEnum entry never runs before the test is run. for some reason QueryTypeImpl is initialised before the test, so it passes

---
+++ queryall-lib/src/main/java/org/queryall/impl/rdfrule/SparqlNormalisationRuleImplFactory.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.queryall.impl.rdfrule;
+
+import org.kohsuke.MetaInfServices;
+import org.queryall.api.rdfrule.NormalisationRuleEnum;
+import org.queryall.api.rdfrule.NormalisationRuleFactory;
+import org.queryall.api.rdfrule.NormalisationRuleParser;
+
+/**
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+@MetaInfServices
+public class SparqlNormalisationRuleImplFactory implements NormalisationRuleFactory
+{
+    /**
+     * Returns the enumeration from the enumeration that matches this factory
+     */
+    @Override
+    public NormalisationRuleEnum getEnum()
+    {
+        return NormalisationRuleEnum.valueOf(SparqlNormalisationRuleImpl.class.getName());
+    }
+    
+    /**
+     * Returns the parser from the enumeration that matches this factory
+     */
+    @Override
+    public NormalisationRuleParser getParser()
+    {
+        return new SparqlNormalisationRuleImplParser();
+    }
+    
+}
