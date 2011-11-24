6129d4158b4f87a5b818af12f1f12ef274f3678e
Peter Ansell <p_ansell@yahoo.com>
2011-11-24 12:13:39 +1000

    javadoc SparqlNormalisationRule

--- queryall-api/src/main/java/org/queryall/api/rdfrule/SparqlNormalisationRule.java
+++ queryall-api/src/main/java/org/queryall/api/rdfrule/SparqlNormalisationRule.java
@@ -7,12 +7,35 @@ import java.util.List;
  */
 public interface SparqlNormalisationRule extends NormalisationRule
 {
-    
+    /**
+     * All SPARQL queries support a Where pattern, so it is included here.
+     * 
+     * More than one Where pattern may be used by a single rule, to easily support different rules
+     * mapping to the same output.
+     * 
+     * @param sparqlWherePattern
+     *            The Basic Graph Pattern string representing one of the Where patterns supported by
+     *            this rule.
+     */
     void addSparqlWherePattern(String sparqlWherePattern);
     
+    /**
+     * 
+     * @return The list of prefix declarations in an N3 compatible string.
+     */
     String getSparqlPrefixes();
     
+    /**
+     * 
+     * @return A list of the SPARQL Where patterns that make up this rule. Each pattern represents
+     *         one of the mappings that make up this rule.
+     */
     List<String> getSparqlWherePatterns();
     
+    /**
+     * 
+     * @param sparqlPrefixes
+     *            A string representing a prefix declaration in the N3 RDF format.
+     */
     void setSparqlPrefixes(String sparqlPrefixes);
 }
