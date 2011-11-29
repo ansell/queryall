970a96036aa3190f4cb2fa86c544690a7d832a4f
Peter Ansell <p_ansell@yahoo.com>
2011-11-29 11:15:35 +1000

    javadoc StringRuleTest

--- queryall-api/src/main/java/org/queryall/api/ruletest/StringRuleTest.java
+++ queryall-api/src/main/java/org/queryall/api/ruletest/StringRuleTest.java
@@ -8,12 +8,28 @@ package org.queryall.api.ruletest;
  */
 public interface StringRuleTest extends RuleTest
 {
+    /**
+     * 
+     * @return The test input string that is to be used as input for denormalisation test stages and output for renormalisation test stages
+     */
     String getTestInputString();
     
+    /**
+     * 
+     * @return The test output string that is to be used as output for denormalisation test stages and input for renormalisation test stages
+     */
     String getTestOutputString();
     
+    /**
+     * 
+     * @param testInputString The test input string that is to be used as input for denormalisation test stages and output for renormalisation test stages
+     */
     void setTestInputString(String testInputString);
     
+    /**
+     * 
+     * @param testOutputString The test output string that is to be used as output for denormalisation test stages and input for renormalisation test stages
+     */
     void setTestOutputString(String testOutputString);
     
 }
