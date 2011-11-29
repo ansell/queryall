09ed2de83c335d07bddb5159d52482c925d9a96b
Peter Ansell <p_ansell@yahoo.com>
2011-11-29 15:59:16 +1000

    javadoc RdfInputQueryType

--- queryall-api/src/main/java/org/queryall/api/querytype/RdfInputQueryType.java
+++ queryall-api/src/main/java/org/queryall/api/querytype/RdfInputQueryType.java
@@ -8,7 +8,19 @@ package org.queryall.api.querytype;
  */
 public interface RdfInputQueryType extends InputQueryType
 {
+    /**
+     * 
+     * Note: Any unexpected parameters from the results of the SPARQL SELECT query will not be usable. All parameters need to be defined before use.
+     * 
+     * @return A SPARQL SELECT statement that is used to populate the expected input parameters for this query
+     */
     String getSparqlInputSelect();
     
+    /**
+     * 
+     * Note: Any unexpected parameters from the results of the SPARQL SELECT query will not be usable. All parameters need to be defined before use.
+     * 
+     * @param sparqlInputSelect A SPARQL SELECT statement that is used to populate the expected input parameters for this query
+     */
     void setSparqlInputSelect(String sparqlInputSelect);
 }
