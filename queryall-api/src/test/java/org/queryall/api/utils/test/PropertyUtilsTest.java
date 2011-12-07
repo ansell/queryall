b5247a3099adef0ff8f803c7123b0ec5bdfeb186
Peter Ansell <p_ansell@yahoo.com>
2011-12-07 16:10:53 +1000

    add test for System.setProperty and System.clearProperty in relation to PropertyUtils.getSystemOrPropertyString

--- queryall-api/src/test/java/org/queryall/api/utils/test/PropertyUtilsTest.java
+++ queryall-api/src/test/java/org/queryall/api/utils/test/PropertyUtilsTest.java
@@ -55,6 +55,39 @@ public class PropertyUtilsTest
     }
     
     /**
+     * Tests the getSystemOrPropertyString using the basic ontology URI properties that are very
+     * stable and should not change inside queryall tests without notice here
+     * 
+     * Test method for
+     * {@link org.queryall.api.utils.PropertyUtils#getSystemOrPropertyString(java.lang.String, java.lang.String)}
+     * .
+     */
+    @Test
+    public final void testGetSystemOrPropertyStringCustomSystem()
+    {
+        // setProperty returns the previous value, so test that it was not set previously
+        Assert.assertNull(System.setProperty("queryall.ontologyPrefix", "somethingreallyrandom"));
+        Assert.assertNull(System.setProperty("queryall.ontologySuffix", "nothingspecial"));
+        
+        // test both with and without a default
+        Assert.assertEquals("Ontology URI Prefix was not as expected", "somethingreallyrandom",
+                PropertyUtils.getSystemOrPropertyString("queryall.ontologyPrefix", ""));
+        Assert.assertEquals("Ontology URI Prefix was not as expected, even with default", "somethingreallyrandom",
+                PropertyUtils.getSystemOrPropertyString("queryall.ontologyPrefix", "http://purl.org/queryall/"));
+        
+        Assert.assertEquals("Ontology URI Suffix was not as expected", "nothingspecial",
+                PropertyUtils.getSystemOrPropertyString("queryall.ontologySuffix", ""));
+        Assert.assertEquals("Ontology URI Suffix was not as expected, even with default", "nothingspecial",
+                PropertyUtils.getSystemOrPropertyString("queryall.ontologySuffix", ":"));
+
+        // clearProperty returns the previous value, so test that it matches what we expect
+        Assert.assertEquals("somethingreallyrandom", System.clearProperty("queryall.ontologyPrefix"));
+        Assert.assertEquals("nothingspecial", System.clearProperty("queryall.ontologySuffix"));
+    }
+    
+    
+    
+    /**
      * Checks that the Property Utils bundle has not been changed without an explicit update here
      */
     @Test
