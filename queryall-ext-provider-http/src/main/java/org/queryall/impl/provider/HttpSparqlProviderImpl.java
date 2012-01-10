4a37c54e06470b1cc7022303e40562970c95c0c4
Peter Ansell <p_ansell@yahoo.com>
2012-01-10 16:27:22 +1000

    cleanup queryall-ext-provider-http

--- queryall-ext-provider-http/src/main/java/org/queryall/impl/provider/HttpSparqlProviderImpl.java
+++ queryall-ext-provider-http/src/main/java/org/queryall/impl/provider/HttpSparqlProviderImpl.java
@@ -18,12 +18,12 @@ import org.slf4j.LoggerFactory;
 
 public class HttpSparqlProviderImpl extends HttpProviderImpl implements HttpSparqlProvider
 {
-    private static final Logger log = LoggerFactory.getLogger(HttpSparqlProviderImpl.class);
-    private static final boolean TRACE = HttpSparqlProviderImpl.log.isTraceEnabled();
+    private static final Logger LOG = LoggerFactory.getLogger(HttpSparqlProviderImpl.class);
+    private static final boolean TRACE = HttpSparqlProviderImpl.LOG.isTraceEnabled();
     @SuppressWarnings("unused")
-    private static final boolean DEBUG = HttpSparqlProviderImpl.log.isDebugEnabled();
+    private static final boolean DEBUG = HttpSparqlProviderImpl.LOG.isDebugEnabled();
     @SuppressWarnings("unused")
-    private static final boolean INFO = HttpSparqlProviderImpl.log.isInfoEnabled();
+    private static final boolean INFO = HttpSparqlProviderImpl.LOG.isInfoEnabled();
     
     private static final Set<URI> HTTP_SPARQL_PROVIDER_IMPL_TYPES = new HashSet<URI>();
     
@@ -58,7 +58,7 @@ public class HttpSparqlProviderImpl extends HttpProviderImpl implements HttpSpar
         {
             if(HttpSparqlProviderImpl.TRACE)
             {
-                HttpSparqlProviderImpl.log.trace("HttpSparqlProviderImpl: nextStatement: " + nextStatement.toString());
+                HttpSparqlProviderImpl.LOG.trace("HttpSparqlProviderImpl: nextStatement: " + nextStatement.toString());
             }
             
             if(nextStatement.getPredicate().equals(RDF.TYPE)
@@ -66,7 +66,7 @@ public class HttpSparqlProviderImpl extends HttpProviderImpl implements HttpSpar
             {
                 if(HttpSparqlProviderImpl.TRACE)
                 {
-                    HttpSparqlProviderImpl.log.trace("HttpSparqlProviderImpl: found valid type predicate for URI: "
+                    HttpSparqlProviderImpl.LOG.trace("HttpSparqlProviderImpl: found valid type predicate for URI: "
                             + keyToUse);
                 }
                 
@@ -123,15 +123,15 @@ public class HttpSparqlProviderImpl extends HttpProviderImpl implements HttpSpar
     }
     
     @Override
-    public void setSparqlGraphUri(final String sparqlGraphUri)
+    public void setSparqlGraphUri(final String nextSparqlGraphUri)
     {
-        this.sparqlGraphUri = sparqlGraphUri;
+        this.sparqlGraphUri = nextSparqlGraphUri;
     }
     
     @Override
-    public void setUseSparqlGraph(final boolean useSparqlGraph)
+    public void setUseSparqlGraph(final boolean nextUseSparqlGraph)
     {
-        this.useSparqlGraph = useSparqlGraph;
+        this.useSparqlGraph = nextUseSparqlGraph;
     }
     
 }
